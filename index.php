67bc6b488abe6e5c95cd13a7eaec9b32998ed815
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-09 20:42:38 +0300

    index with include

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="resources/css/materialize.css"  media="screen,projection"/>
+
+    <script type="text/javascript" src="resources/js/jquery-3.4.1.min.js"></script>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>WebApp</title>
+</head>
+<body>
+<h1>HI</h1>
+
+
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="resources/js/materialize.js"></script>
+</body>
+</html>
