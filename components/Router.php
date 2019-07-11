5b88a736b2de2c95082974dd30e395e46a0ed263
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-11 12:41:20 +0300

    comments for router

--- components/Router.php
+++ components/Router.php
@@ -23,19 +23,26 @@ class Router
 
     public function run(){
 
+        /*Получаем строку запроса, куда пришел пользователь*/
         $url = $this->getURL();
 
+        /*разбираем роутер конфиг и проходим по ниму до конца*/
         foreach ($this->routes as $uriPattern=>$path){
+
+            /*Если хотя бы один паттерн из роутера совпал со строкой запроса,
+             то создаем контроллер(и вызываем экшин)*/
             if (preg_match("~\b$uriPattern\b~", $url)){
+
+                //разбиваем полученный путь на массив (контроллер и экшин)
                 $segments = explode('/',$path);
-                $controllerName = array_shift($segments)."Controller";
-                $actionName = "action".ucfirst(array_shift($segments));
+                $controllerName = array_shift($segments)."Controller"; //название класса для контроллера
+                $actionName = "action".ucfirst(array_shift($segments)); //название метода для экшина
 
 
-                $controllersFileName = ROOT."/controller/$controllerName.php";
-                if (file_exists($controllersFileName)){include_once($controllersFileName);}
+                $controllersFileName = ROOT."/controller/$controllerName.php"; //ищем класс контроллера
+                if (file_exists($controllersFileName)){include_once($controllersFileName);} //подключаем
 
-
+                /*Создаем класс контроллера и запускаем экшин*/
                 $objectController = new $controllerName;
                 $result = $objectController->$actionName();
                 if ($result!=null) break;
